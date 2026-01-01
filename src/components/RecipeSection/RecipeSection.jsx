68db28b7b3db05431b167d2f256ca0db03de8e13
Arav3103 <arav3103@gmail.com>
2026-01-01 20:06:43 +0530

    feat: Added Home sections and Quick Filters array - 01/01/2026

--- src/components/RecipeSection/RecipeSection.jsx
+++ src/components/RecipeSection/RecipeSection.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import CreateRecipe from "./CreateRecipe";
 import DisplayRecipes from "./DisplayRecipes";
+import { quickFilters } from "../../constants";
 
 const RecipeSection = () => {
   const [recipeList, setRecipeList] = useState([]);
   return (
     <main>
       <section id="recipes-home">
+        <section id="hero">
+          <h1>What to cook today?</h1>
+          <input
+            type="text"
+            name="wtcInput"
+            id="wtcInput"
+            placeholder="Sambar, Tomato, Brinjal, Kurma..."
+          />
+          <button name="wtcCtaBtn" id="wtcCtaBtn">Find</button>
+        </section>
+        <section>
+          <h2>Quick Filters</h2>
+          <div>{quickFilters}</div>
+        </section>
         <aside id="create-recipe">
           <CreateRecipe setRecipeList={setRecipeList} recipeList={recipeList} />
         </aside>
+
         <section id="display-recipe">
           <DisplayRecipes
             setRecipeList={setRecipeList}
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,11 @@
 export const DB_NAME = "recipe-app-db";
 export const DB_VERSION = 1;
+export const quickFilters = [
+  "South Indian",
+  "North Indian",
+  "Breakfast",
+  "Lunch",
+  "Dinner",
+  "Veg",
+  "Non-veg",
+];
